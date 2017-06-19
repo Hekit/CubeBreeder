1825e85a2d7efa9382645f4095c60ae47f9bb2a9
Hekit <kubondavid@seznam.cz>
2017-06-19 14:37:13 +0200

    added selectors

---
+++ CubeBreeder/Selectors/RouletteWheelSelector.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CubeBreeder.Selectors
+{
+    class RouletteWheelSelector : Selector
+    {
+        RandomNumberGenerator rng = RandomNumberGenerator.GetInstance();
+
+        public void Select(int howMany, Population from, Population to)
+        {
+            double fitnessSum = 0.0;
+
+            for (int i = 0; i < from.GetPopulationSize(); i++)
+            {
+                fitnessSum += from.Get(i).GetFitnessValue();
+            }
+
+            double[] fitnesses = new double[from.GetPopulationSize()];
+
+            for (int i = 0; i < fitnesses.Length; i++)
+            {
+                fitnesses[i] = from.Get(i).GetFitnessValue() / fitnessSum;
+            }
+
+            for (int i = 0; i < howMany; i++)
+            {
+
+                double ball = rng.NextDouble();
+                double sum = 0;
+
+                for (int j = 0; j < fitnesses.Length; j++)
+                {
+
+                    sum += fitnesses[j];
+                    if (sum > ball)
+                    {
+                        to.Add((Individual)from.Get(j).Clone());
+                        break;
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ CubeBreeder/Selectors/TournamentSelector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CubeBreeder.Selectors
+{
+    class TournamentSelector : Selector
+    {
+        RandomNumberGenerator rng = RandomNumberGenerator.GetInstance();
+
+        public void Select(int howMany, Population from, Population to)
+        {
+
+            for (int i = 0; i < howMany; i++)
+            {
+                int i1 = rng.NextInt(from.GetPopulationSize());
+                int i2 = rng.NextInt(from.GetPopulationSize());
+
+                if ((from.Get(i1).GetFitnessValue() > from.Get(i2).GetFitnessValue()) && rng.NextDouble() < 0.8)
+                {
+                    to.Add((Individual)from.Get(i1).Clone());
+                }
+                else
+                {
+                    to.Add((Individual)from.Get(i2).Clone());
+                }
+            }
+        }
+    }
+}
