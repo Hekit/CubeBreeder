6fd777dd2ebdf81a9fefad0639c617ca8dfa1fcb
Hekit <kubondavid@seznam.cz>
2017-06-19 15:42:09 +0200

    testing of individuals

--- CubeBreederTests/IndividualTest.cs
+++ CubeBreederTests/IndividualTest.cs
@@ -20,6 +20,16 @@ namespace CubeBreederTests
         public void TestMethod2()
         {
             Individual ind = new Individual(gi);
+            ind.RandomInitialization();
+            Individual ind1 = new Individual(ind);
+            Individual ind2 = (Individual)ind.Clone();
+            for (int i = 0; i < ind.Length(); i++)
+            {
+                Assert.AreEqual(ind.IsActiveOnEdge(i), ind2.IsActiveOnEdge(i), i.ToString());
+            }
+            ind.SetActivityBetweenVertices(0, 1, true);
+            ind2.SetActivityBetweenVertices(0, 1, false);
+            Assert.AreNotEqual(ind.IsActiveBetweenVertices(0,1), ind2.IsActiveBetweenVertices(0,1), "Two individuals but same object!");
         }
     }
 }
